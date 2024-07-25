c882572a04927a5f92e73c2980046fd08ad0f76c
Nicholas Lorenzetti <nicholaslorenzetti@Nicholass-MBP-2.lan>
2024-07-25 23:38:42 +0930

    fifth test

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -1,6 +1,6 @@
 bool is_fanarray(int array[], int n) {
-    if (n < 1 || (n % 2) == 0) {
-        return 0;
+    if (n < 1) {
+        return false;
     }
     for (int i = 0; i < n; i++) {
         if (array[i] != array[(n-1)-i]) {
