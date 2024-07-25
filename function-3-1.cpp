d7a75da7168f76af296441ce299bae3b9444a203
Nicholas Lorenzetti <nicholaslorenzetti@Nicholass-MBP-2.lan>
2024-07-25 23:53:32 +0930

    eigth test

--- function-3-1.cpp
+++ function-3-1.cpp
@@ -2,7 +2,7 @@ bool is_fanarray(int array[], int n) {
     if (n < 1) {
         return false;
     }
-    for (int i = 1; i < n / 2; i++) { 
+    for (int i = 1; i <= n / 2; i++) { 
         if (array[i] < array[i-1]) {
             return false;
         }
