e5e1670f16ce64d41d323b9b0229b63a1611e220
Nicholas Lorenzetti <nicholaslorenzetti@Nicholass-MBP-2.lan>
2024-07-25 23:31:13 +0930

    Second test

--- function-3-2.cpp
+++ function-3-2.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
 int median_array(int array[], int n) {
-    std::sort(array, array+n);
-    for (int i = 0; i < n; i++) {
-    std::cout << "Array element " << i << " is: " << array[i] << std::endl;
+    int median;
+    if (n < 1 || (n % 2) == 0) {
+        return 0;
     }
+    std::sort(array, array+n);
+    median = array[n/2];
+    return median;
 }
--- main-3-2.cpp
+++ main-3-2.cpp
@@ -3,6 +3,6 @@
 extern int median_array(int array[], int n);
 
 int main() {
-    int array[5] = {1, 4, 6, 3, 2};
-    median_array(array, 5);
+    int array[7] = {1, 4, 6, 5, 2, 7, 8};
+    std::cout << "The array median is: " << median_array(array, 7) << std::endl;
 }
