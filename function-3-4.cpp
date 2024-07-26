2a32d4517ddf49791f772f8363c065320c240325
Nicholas Lorenzetti <nicholaslorenzetti@Nicholass-MBP-2.lan>
2024-07-26 21:42:23 +0930

    tricky problems

---
+++ function-3-4.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+void print_pass_fail(char grade) {
+    switch (grade) {
+        case 'A': {
+            std::cout << "Pass \n" << std::endl;
+            break;
+        }
+        case 'B': {
+            std::cout << "Pass \n" << std::endl;
+            break;
+        }
+        case 'C': {
+            std::cout << "Pass \n" << std::endl;
+            break;
+        }
+        case 'D': {
+            std::cout << "Fail \n" << std::endl;
+            break;
+        }
+        case 'E': {
+            std::cout << "Fail \n" << std::endl;
+        }
+    }
+}
