fcd74eb1aa23d5f1e93d7386b6545a3507a3497e
Nicholas Lorenzetti <nicholaslorenzetti@Nicholass-MacBook-Pro-2.local>
2024-08-14 16:04:11 +0930

    Second

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -4,6 +4,6 @@
 PersonList deepCopyPersonList(PersonList pl) {
     PersonList field1 = pl;
     PersonList field2;
-    field2 = field1;
+    field2.people = field1.people;
     return field2;
 }
--- main-1-4.cpp
+++ main-1-4.cpp
@@ -2,15 +2,10 @@
 #include "Person.h"
 
 extern PersonList shallowCopyPersonList(PersonList pl);
+extern PersonList createPersonList(int n);
 
 int main() {
-    PersonList field1;
-    field1.numPeople = 3;
-    Person * arr = new Person[3];
-    field1.people = arr;
-    for (int i = 0; i < 3; i++) {
-    arr[i] = {"James Doe", 2};
-    }
+    PersonList field1 = createPersonList(3);
     PersonList field2 = shallowCopyPersonList(field1);
     std::cout << field2.people[1].name << " " << field2.people[1].age << std::endl;
 }
