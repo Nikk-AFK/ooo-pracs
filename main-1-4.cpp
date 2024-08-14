0054f55ae1ea38a9c3b08cd3e0db1e94f6c2aeb6
Nicholas Lorenzetti <nicholaslorenzetti@Nicholass-MacBook-Pro-2.local>
2024-08-14 16:12:05 +0930

    Third

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -2,11 +2,18 @@
 #include "Person.h"
 
 extern PersonList shallowCopyPersonList(PersonList pl);
-extern PersonList createPersonList(int n);
 
 int main() {
-    PersonList field1 = createPersonList(3);
+    PersonList field1;
+    field1.numPeople = 3;
+    Person * arr = new Person[3];
+    field1.people = arr;
+    for (int i = 0; i < 3; i++) {
+    arr[i] = {"Jane Doe", 1};
+    }
+
     PersonList field2 = shallowCopyPersonList(field1);
-    std::cout << field2.people[1].name << " " << field2.people[1].age << std::endl;
+    std::cout << field2.people[0].name << " " << field2.people[0].age << std::endl;
+    std::cout << field1.people[2].name << " " << field1.people[2].age << std::endl;
 }
 
