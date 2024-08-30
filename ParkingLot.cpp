3c307456c7969a933392dc6e6ae9ae661ccb75b3
Nicholas Lorenzetti <nicholaslorenzetti@Nicholass-MBP-2.lan>
2024-08-31 01:01:00 +0930

    f i x e s 3

---
+++ ParkingLot.cpp
@@ -0,0 +1,32 @@
+#include "ParkingLot.h"
+
+ParkingLot::ParkingLot(int max) : max(max), count(0), vehicles(new Vehicle[max]) {}
+ParkingLot::ParkingLot() : max(10), count(0), vehicles(new Vehicle[10]) {}
+
+int ParkingLot::getCount() {
+    return count;
+}
+
+void ParkingLot::parkVehicle(Vehicle obj) {
+    if (count < max) {
+        vehicles[count] = obj;
+        count++;
+    } else {
+        std::cout << "The lot is full" << std::endl;
+    }
+}
+
+void ParkingLot::unparkVehicle(int id) {
+    bool result = false;
+    for (int i = 0; i < count; i++) {
+        if (vehicles[i].getID() == id) {
+            vehicles[i] = 0;
+            count--;
+            std::cout << "Vehicle removed" << std::endl;
+            result = true;
+        }
+    }
+    if (result == false) {
+        std::cout << "Vehicle not in the lot" << std::endl;
+    }
+}
---
+++ ParkingLot.h
@@ -0,0 +1,23 @@
+#ifndef PARKINGLOT_H
+#define PARKINGLOT_H
+
+#include "Vehicle.h"
+#include "Car.h"
+#include "Bus.h"
+#include "Motorbike.h"
+
+class ParkingLot {
+    private:
+    int max;
+    int count;
+
+    public: 
+    Vehicle * vehicles;
+    ParkingLot(int max);
+    ParkingLot();
+    int getCount();
+    void parkVehicle(Vehicle obj);
+    void unparkVehicle(int id);
+};
+
+#endif
