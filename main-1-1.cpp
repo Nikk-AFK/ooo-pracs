655b648e1fc88480ac3ef08f3f460f97f9910130
Nicholas Lorenzetti <nicholaslorenzetti@Nicholass-MacBook-Pro-2.local>
2024-09-04 15:26:35 +0930

    Q1 f

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -3,13 +3,13 @@
 
 int main() {
     AirCraft plane(10);
-    plane.set_fuel(50);
-    std::cout << "fuel: " << plane.get_fuel() << std::endl;
+    plane.set_fuel(53.54545);
+    std::cout << "fuel: " << float(plane.get_fuel()) << std::endl;
     plane.refuel();
     std::cout << "refueled. now fuel: " << plane.get_fuel() << std::endl;
     plane.fly(10, 5);
     plane.set_numberOfFlights(5);
-    std::cout << "no. of flights: " << plane.get_numberOfFlights() << std::endl;
+    std::cout << "no. of flights: "<< plane.get_numberOfFlights() << std::endl;
     plane.set_weight(plane.get_weight() + 5);
     std::cout << "weight is now: " << plane.get_weight() << std::endl;
 }
