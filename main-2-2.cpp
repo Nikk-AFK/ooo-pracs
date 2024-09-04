ea21e904a7a5be81d95ba0bc76f724e600738693
Nicholas Lorenzetti <nicholaslorenzetti@Nicholass-MacBook-Pro-2.local>
2024-09-04 16:43:26 +0930

    Q3 f5

--- main-2-2.cpp
+++ main-2-2.cpp
@@ -4,11 +4,11 @@
 int main() {
     Airplane Boeing(1000, 100);
     Boeing.refuel();
-    Boeing.set_fuel(80);
+    Boeing.set_fuel(95);
     std::cout << "weight is now: " << Boeing.get_weight() << std::endl;
     std::cout << "now fuel: " << Boeing.get_fuel() << std::endl;
     Boeing.set_numberOfFlights(5);
-    Boeing.fly(0, 45);
+    Boeing.fly(90, 120);
     std::cout << "no. of flights: "<< Boeing.get_numberOfFlights() << std::endl;
     std::cout << "fuel is now: " << Boeing.get_fuel() << std::endl;
 }
