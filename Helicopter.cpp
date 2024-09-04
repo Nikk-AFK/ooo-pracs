30753f787c13f4ee4d583cd989214f4b1b5a5f53
Nicholas Lorenzetti <nicholaslorenzetti@Nicholass-MacBook-Pro-2.local>
2024-09-04 16:17:15 +0930

    Q2 f3

--- Helicopter.cpp
+++ Helicopter.cpp
@@ -1,9 +1,6 @@
 #include "Helicopter.h"
 
-    Helicopter::Helicopter(int w, std::string n) : AirCraft(w), name(n) {
-        set_fuel(100);
-        set_numberOfFlights(0);
-    };
+    Helicopter::Helicopter(int w, std::string n) : AirCraft(w), name(n) {};
     std::string Helicopter::get_name() {
         return name;
     }
@@ -14,13 +11,13 @@
         float fuel_usage;
         if (headwind >= 40) {
             if (get_weight() > 5670) {
-                fuel_usage = ((0.4 + ((get_weight() - 5670)*0.001*minutes)) * minutes);
+                fuel_usage = (0.4 + ((get_weight() - 5670)*(0.001))) * minutes;
             } else {
                 fuel_usage = (0.4 * minutes);
             }
         } else {
             if (get_weight() > 5670) {
-                fuel_usage = ((0.2 + ((get_weight() - 5670)*0.001*minutes)) * minutes);
+                fuel_usage = (0.2 + ((get_weight() - 5670)*(0.001))) * minutes;
             } else {
                 fuel_usage = (0.2 * minutes);
             }
@@ -28,7 +25,6 @@
         if ((get_fuel() - fuel_usage) < 20) {
         } else {
             set_fuel((get_fuel() - fuel_usage));
-            int num = get_numberOfFlights();
-            set_numberOfFlights(num++);
+            set_numberOfFlights((get_numberOfFlights()) + 1);
         }
     }
--- main-2-1.cpp
+++ main-2-1.cpp
@@ -2,13 +2,14 @@
 #include "Helicopter.h"
 
 int main() {
-    Helicopter Copter(5690, "Chopper");
+    Helicopter Copter(6190, "Chopper");
     Copter.refuel();
     Copter.set_fuel(80);
+    std::cout << "weight is now: " << Copter.get_weight() << std::endl;
     std::cout << "now fuel: " << Copter.get_fuel() << std::endl;
     std::cout << "name: " << Copter.get_name() << std::endl;
     Copter.set_numberOfFlights(5);
-    Copter.fly(0, 10);
+    Copter.fly(0, 45);
     std::cout << "no. of flights: "<< Copter.get_numberOfFlights() << std::endl;
     std::cout << "fuel is now: " << Copter.get_fuel() << std::endl;
 }
