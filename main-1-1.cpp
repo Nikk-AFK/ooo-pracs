cdf0e7c428d4e7e535f6a2eac7788e05de4cf1e7
Nicholas Lorenzetti <nicholaslorenzetti@Nicholass-MacBook-Pro-2.local>
2024-09-04 15:23:22 +0930

    Q1

--- main-1-1.cpp
+++ main-1-1.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
-#include "Appliance.h"
+#include "AirCraft.h"
 
 int main() {
-    Appliance dishwasher(5);
-    Appliance kettle;
-    dishwasher.turnOn();
-    kettle.turnOn();
-    std::cout << "The dishwasher has a power rating of " << dishwasher.get_powerRating() << std::endl;
-    dishwasher.set_powerRating(3);
-    std::cout << "The dishwasher now has a power rating of " << dishwasher.get_powerRating() << std::endl;
-    dishwasher.turnOff();
-    std::cout << "The dishwasher is currently " << dishwasher.get_isOn() << std::endl;
+    AirCraft plane(10);
+    plane.set_fuel(50);
+    std::cout << "fuel: " << plane.get_fuel() << std::endl;
+    plane.refuel();
+    std::cout << "refueled. now fuel: " << plane.get_fuel() << std::endl;
+    plane.fly(10, 5);
+    plane.set_numberOfFlights(5);
+    std::cout << "no. of flights: " << plane.get_numberOfFlights() << std::endl;
+    plane.set_weight(plane.get_weight() + 5);
+    std::cout << "weight is now: " << plane.get_weight() << std::endl;
 }
