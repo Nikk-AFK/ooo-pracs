d80e51ed06e1ebb21f86c28163390c85fd6b8ee0
Nicholas Lorenzetti <nicholaslorenzetti@Nicholass-MacBook-Pro-2.local>
2024-10-23 16:04:49 +1030

    oh wow

--- GridItem.hpp
+++ GridItem.hpp
@@ -34,7 +34,9 @@ class GridItem {
         return count;
     }
 
-    virtual ~GridItem() {}
+    virtual ~GridItem() {
+        count--;
+    }
 
     protected:
     std::pair<int, int> position;
