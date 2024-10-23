b49c064de5a7b7d21d80bd14248b5682b2ae9f4e
Nicholas Lorenzetti <nicholaslorenzetti@Nicholass-MacBook-Pro-2.local>
2024-10-23 15:47:17 +1030

    pls

--- main-1.cpp
+++ main-1.cpp
@@ -3,8 +3,8 @@
 #include "GridItem.hpp"
 #include "Helper.hpp"
 int main() {
-    GridItem item1(4, 3, 2, 5);
-    GridItem item2(7, 9, 2, 5);
+    GridItem item1(4, 3, 1, 5);
+    GridItem item2(7, 9, 1, 5);
 
     int dist = Helper::manhattanDistance(item1.getCoordinates(), item2.getCoordinates());
     std::cout << "Manhattan distance is: " << dist << std::endl;
